05e8f1e2dbaef7f3448f9d1da905a7f15f9f2aa0
Zhengyu-Huang <hzyhzy2014@gmail.com>
2017-10-29 22:41:32 -0700

    add plot for kuhn simplex

--- KuhnSimplex.py
+++ KuhnSimplex.py
@@ -64,7 +64,7 @@ class KuhnSimplex:
         '''
         nx, ny, nz = self.nx, self.ny, self.nz
 
-        eles = np.empty((6*(nx - 1) * (ny - 1) * (nz - 1), 4))
+        eles = np.empty((6*(nx - 1) * (ny - 1) * (nz - 1), 4), dtype=int)
         for k in range(nz - 1):
             for j in range(ny - 1):
                 for i in range(nx - 1):
@@ -133,12 +133,87 @@ class KuhnSimplex:
         write_tet(nodes, eles, boundaries)
 
 
+    def plot_mesh(self):
+        import matplotlib.pyplot as plt
+        from mpl_toolkits.mplot3d import Axes3D
+        fig = plt.figure()
+        ax = fig.add_subplot(111, projection='3d')
 
+        eles  = self.eles
+        nodes = self.nodes
+        for e in eles:
+            for i in range(4):
+                for j in range(i+1,4):
+                    x = [nodes[e[i],0],nodes[e[j],0]]
+                    y = [nodes[e[i],1],nodes[e[j],1]]
+                    z = [nodes[e[i],2],nodes[e[j],2]]
+                    ax.plot(x,y,z)
+        plt.show()
+
+
+
+
+
+
+def geomspace(x0, xn, dx0, ratio, includeX0 = True):
+    xx = [x0] if includeX0 else []
+    x, dx = x0, dx0
+
+    while (x0 < xn and x < xn) or (x0 > xn and x > xn):
+        dx *= ratio
+        x = x + dx
+        xx.append(x)
+
+
+
+    return xx
+
+
+def parachute3D():
+    # x direction, the parachute is in [-7.7235, 7.7235]
+    # the fluid domain is [-150 150]
+    xc = np.linspace(-10.0, 10.0, 41)
+    dxc = 20.0/40.0
+    xRatio = 1.5
+    xl = geomspace(-10.0, -150, -dxc, xRatio, False)
+    xr = geomspace(10.0, 150, dxc, xRatio, False)
+
+
+    x = [*reversed(xl),*xc,*xr]
+
+
+    y = x
+    # z direction, the parachute is in [35.7358, 39.2198]
+    # the fluid domain is [-100 200]
+    zc = np.linspace(32.0, 52.0, 41)
+    dzc = 20.0 / 40.0
+    zRatio = 1.5
+    zl = geomspace(32.0, -100, -dzc, zRatio, False)
+    zr = geomspace(52.0, 150, dzc, zRatio, False)
+    z = [*reversed(zl), *zc, *zr]
+
+    simpleKuhnSimplex = KuhnSimplex(x,y,z)
+
+    print('Writing to top file')
+    simpleKuhnSimplex.write_topfile()
+
+def naca2D():
+    x = np.linspace(-1.0, 2.0, 40)
+    y = np.linspace(-0.5, 0.5, 40)
+    z = [0.0, 0.01, 0.02, 0.03]
+
+    simpleKuhnSimplex = KuhnSimplex(x,y,z)
+
+    print('Writing to top file')
+    simpleKuhnSimplex.write_topfile()
 
 
 if __name__ == '__main__':
-    x = [0, 1, 2, 4]
-    y = [0, 3, 4, 5]
-    z = [0, 2, 6, 7]
+    x = [1,2]
+    y = [4,5]
+    z = [7,8]
+
     simpleKuhnSimplex = KuhnSimplex(x,y,z)
-    simpleKuhnSimplex.write_topfile()
+
+    print('Writing to top file')
+    simpleKuhnSimplex.plot_mesh()
