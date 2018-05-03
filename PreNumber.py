3e5f44bc8522c9ccc6762748df2efecd64e6424e
Zhengyu-Huang <hzyhzy2014@gmail.com>
2018-05-02 19:05:20 -0700

    add PreNum

---
+++ PreNumber.py
@@ -0,0 +1,235 @@
+'''
+Alkämper, Martin, Fernando Gaspoz, and Robert Klöfkorn.
+"A Weak Compatibility Condition for Newest Vertex Bisection in any dimension."
+arXiv preprint arXiv:1711.03141 (2017).
+'''
+from TopFileTool import write_tet, read_tet
+from Utility import pair_sort,triplet_sort
+import numpy as np
+import operator
+
+
+
+def edge_in_face(edge, face):
+   if edge[0] in set(face) and edge[1] in set(face):
+       return True
+   else:
+       return False
+
+
+
+
+
+
+class PreNumber:
+    def __init__(self,inputfile,outputfile):
+        self.nodes, self.eles, self.boundaryNames, self.boundaries = read_tet(inputfile)
+        self._build_faces()
+        self._mark_edge()
+        self._global_ordering()
+        self._renumber()
+        self.write_topfile(outputfile)
+
+
+
+    def _build_faces(self):
+        eles = self.eles
+        faces = {}
+        for ei in range(len(eles)):
+            e = eles[ei]
+            for i in range(4):
+                enen = [e[j] for j in range(4) if (j != i)]
+
+                if triplet_sort(enen[0], enen[1], enen[2]) in faces:
+                    faces[triplet_sort(enen[0], enen[1], enen[2])].append(ei)
+                else:
+                    faces[triplet_sort(enen[0], enen[1], enen[2])] = [ei]
+
+        self.faces = faces
+
+    def _check_on_boundary(self, fp):
+        faces = self.faces
+        es = faces[fp]
+        return True if len(es) == 1 else False
+
+    def _mark_edge(self):
+        '''
+        for each element,
+        mark its longest edge, if it has multiple longest edges,
+        mark the one with largest value, i + j,
+        here i, j are the two node ids
+        '''
+
+        eles = self.eles
+        nodes = self.nodes
+        refineEdges = []
+
+        for e in eles: #loop all elements
+            maxLen, maxSum = -1.0, 0
+            for i in range(4):
+                for j in range(i):
+                    ni, nj = e[i], e[j]
+                    len = np.sqrt((nodes[ni][0] - nodes[nj][0])**2 + (nodes[ni][1] - nodes[nj][1])**2 + (nodes[ni][2] - nodes[nj][2])**2)
+                    sum = ni + nj
+                    if(len > maxLen or (len == maxLen and sum > maxSum)):
+                        refineEdge = pair_sort(ni, nj)
+
+            refineEdges.append(refineEdge)
+
+        self.refineEdges = refineEdges
+
+    def _refine_edge(self, ele, nodeMap):
+        min, max = np.inf, -1
+        for n in ele:
+            if nodeMap[n] < min:
+                min = nodeMap[n]
+                n0 = n
+            if nodeMap[n] > max:
+                max = nodeMap[n]
+                nd = n
+        return [n0,nd]
+
+    def _global_ordering(self):
+        faces = self.faces
+        eles = self.eles
+        refineEdges = self.refineEdges
+
+        ne = len(eles)
+        elementMarker = np.zeros(ne)
+        nodeMap = {}
+        nonBoundaryFaceSet = []
+
+        ### Put the first element
+        elementMarker[0] = 1
+        for fp in [triplet_sort(eles[0][1], eles[0][2], eles[0][3]),
+                   triplet_sort(eles[0][0], eles[0][2], eles[0][3]),
+                   triplet_sort(eles[0][0], eles[0][1], eles[0][3]),
+                   triplet_sort(eles[0][0], eles[0][1], eles[0][2])]:
+            if self._check_on_boundary(fp):
+                continue
+            else:
+                if edge_in_face(refineEdges[0], fp):
+                    nonBoundaryFaceSet.insert(0, fp)
+                else:
+                    nonBoundaryFaceSet.append(fp)
+
+        n0, nd = refineEdges[0]
+        nodeMap[n0] = 0
+        nodeMap[nd] = 3
+        temp = 1
+        for ni in eles[0]:
+            if ni != n0 and ni != nd:
+                nodeMap[ni] = temp
+                temp = temp + 1
+
+
+        while nonBoundaryFaceSet: # the element
+            f = nonBoundaryFaceSet.pop(0)
+            print('face is ', f)
+            es = faces[f]
+            vf1, vf2, vf3 = f
+            for i in range(2):
+                #print('elem marker', len(eles), len(elementMarker), es[1 - i])
+                if(elementMarker[es[1 - i]]):
+                     continue
+
+                v = eles[es[i]][0] + eles[es[i]][1] + eles[es[i]][2] + \
+                    eles[es[i]][3] - vf1 - vf2 - vf3
+
+                vp = eles[es[1 - i]][0] + eles[es[1 - i]][1] + eles[es[1 - i]][2] + \
+                    eles[es[1 - i]][3] - vf1 - vf2 - vf3
+                if vp not in nodeMap: #already been ordered
+                    #print('debug')
+                    #print('>>>', nodeMap[v], nodeMap[vf1], nodeMap[vf2], nodeMap[vf3])
+                    if nodeMap[v] > nodeMap[vf1] and nodeMap[v] > nodeMap[vf2] and nodeMap[v] > nodeMap[vf3]:
+                    #no RefEdge insert before v or after v
+                        nodeMap[vp] = nodeMap[v]
+                    else:#RefEdge insert after v
+                        nodeMap[vp] = nodeMap[v]
+                elementMarker[es[1 - i]] = 1
+
+                #mark T'
+                #loop face of T'
+                n0,nd = self._refine_edge(eles[es[1 - i]], nodeMap)
+                for fp in [triplet_sort(eles[es[1 - i]][1], eles[es[1 - i]][2], eles[es[1 - i]][3]),
+                           triplet_sort(eles[es[1 - i]][0], eles[es[1 - i]][2], eles[es[1 - i]][3]),
+                           triplet_sort(eles[es[1 - i]][0], eles[es[1 - i]][1], eles[es[1 - i]][3]),
+                           triplet_sort(eles[es[1 - i]][0], eles[es[1 - i]][1], eles[es[1 - i]][2])]:
+                    print('fp is ', fp)
+                    if len(faces[fp]) == 1:
+                        continue
+                    if edge_in_face([n0, nd], fp):
+                        nonBoundaryFaceSet.insert(0, fp)
+
+                    else :
+                        nonBoundaryFaceSet.append(fp)
+
+
+        self.nodeMap = nodeMap
+
+
+
+
+
+        
+
+
+
+
+
+
+
+
+
+    def _renumber(self):
+        '''
+        insert a node P on the tetrahedron center, to divide it into three new tetrahedra
+        Each child tetrahedron t are divided into 4 tetrahedron by adding a node Q at the original face center
+
+        The P ----> Z1  Q ---> Z2
+        Original edge A,B if A < B: A ---> Z0 B ---> Z3
+                          if A > B: A ---> Z3 B ---> Z0
+
+        :return:
+        '''
+
+        nodeMap = self.nodeMap
+        eles = self.eles
+
+
+
+        newElems = []
+
+        for e in eles: #loop all elements
+            #the tetrahedron center is P
+            n0,n1,n2,n3 = e
+            tetMap = {n0:nodeMap[n0], n1:nodeMap[n1], n2:nodeMap[n2], n3:nodeMap[n3],}
+            sortedTetMap = sorted(tetMap.items(), key=operator.itemgetter(1))
+            newElems.append([sortedTetMap[0][0], sortedTetMap[1][0], sortedTetMap[2][0], sortedTetMap[3][0]])
+
+            
+
+
+
+        self.newElems = newElems
+
+
+
+    def write_topfile(self, outputfile):
+        nodes = self.nodes
+        eles = self.newElems
+        boundaries = self.boundaries
+        boundaryNames = self.boundaryNames
+        write_tet(nodes, eles, boundaryNames, boundaries,outputfile)
+        print('Be careful, all these elements have tag = 2')
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    #naca2D()
+    naca2D = PreNumber('domain.top','domain_iso.top')
+
